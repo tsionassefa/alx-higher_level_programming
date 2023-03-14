6315515676c7cb9f4afd785463a9e23ca2e8e9c3
tsionassefa <tsionassefa101@gmail.com>
2023-03-14 22:15:05 +0300

    done

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+--  lists all records of the table
+-- Don’t list rows without a name value
+-- display the score and the name
+-- listed by descending score
+SELECT score, name FROM second_table WHERE name IS NOT NULL  ORDER BY scorer DESC;
