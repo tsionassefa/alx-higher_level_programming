bcf2e22b4def09cb7a8b3804b213210048fa8fc5
tsionassefa <tsionassefa101@gmail.com>
2023-03-14 20:49:28 +0300

    done

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- list all records of the table 
 -- only score and name
 -- ordrered by top first score 
-SELECT score, name FROM second_table WHERE score DESC; 
+SELECT score, name FROM second_table ORDER BY score DESC; 
