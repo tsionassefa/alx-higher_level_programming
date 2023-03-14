5e77becfac80c5e6ebff13f563c0211a2b93ea16
tsionassefa <tsionassefa101@gmail.com>
2023-03-14 20:28:32 +0300

    done

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
--- create table
--- not allowed to use the SELECT and SHOW statements
+-- create a table second_table in hbtn_0c_0
+-- add multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
 score INT);
@@ -7,4 +7,4 @@ score INT);
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table(id, name, score) VALUES (4, 'Bob', 8);
+INSERT INTO second_table(id, name, score) VALUES (4, 'George', 8);
