4f185c657a2dff4bd0646b6640a287da77b4b6d2
tsionassefa <tsionassefa101@gmail.com>
2023-03-14 20:26:11 +0300

    done

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,9 @@
 -- create table
 -- not allowed to use the SELECT and SHOW statements
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
+
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14);
