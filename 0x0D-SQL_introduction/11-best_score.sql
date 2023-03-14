ce741cdc80c21b8d903ebcbef881f13b262a0973
tsionassefa <tsionassefa101@gmail.com>
2023-03-14 21:01:36 +0300

    done

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all records with a score >= 10
 -- Results should display both the score and the name
-SELECT score, name FROM second_table WHERE score >= 10, DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
