4f52f2e630a12d7f351bc840d293c2777490f519
tsionassefa <tsionassefa101@gmail.com>
2023-03-14 22:16:40 +0300

    done

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,4 +2,4 @@
 -- Don’t list rows without a name value
 -- display the score and the name
 -- listed by descending score
-SELECT score, name FROM second_table WHERE name IS NOT NULL  ORDER BY scorer DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL  ORDER BY score DESC;
