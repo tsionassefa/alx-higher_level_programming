c2897b51b751803a8fb86d8ce7bb1e4c884464a7
tsionassefa <tsionassefa101@gmail.com>
2023-03-15 12:25:26 +0300

    done

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- creates the table force_name on MYSQL
 -- insert description
-DROP TABLE IF EXISTS 'force_name';
-CREATE TABLE 'force_name' (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `force_name` (
+	`id` INT,
+	`name` VARCHAR(256));
