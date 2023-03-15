12dbe27cb846c9e59dcbae95dfffb3196b2964fa
tsionassefa <tsionassefa101@gmail.com>
2023-03-15 12:31:43 +0300

    done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- create user user_0d_2 with select privillage
 -- password should be set to user_0d_2_pwd
 -- if usr nad dn already exisists script sholdn't fail
-CREATE IF NOT EXISTS  DATABASE  hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTs hbtn_0d_2;
 CREATE USER IF NOT  EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON `hbtn_0d_2`.*
