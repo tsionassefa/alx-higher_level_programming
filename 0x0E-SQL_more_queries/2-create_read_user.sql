dba45c62f8afb84ca43eb407db3bda8a24af12ff
tsionassefa <tsionassefa101@gmail.com>
2023-03-15 11:02:23 +0300

    done

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_2
+-- create user user_0d_2 with select privillage
+-- password should be set to user_0d_2_pwd
+-- if usr nad dn already exisists script sholdn't fail
+CREATE IF EXISTS  DATABASE  hbtn_0d_2;
+CREATE USER IF EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.table TO 'user_0d_2'@'host';
