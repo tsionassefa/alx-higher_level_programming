4fd97f7d3eedadca42098f622b4488075ccb16b2
tsionassefa <tsionassefa101@gmail.com>
2023-04-10 17:54:55 +0300

    done

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// script to print strings using a loop
+
+let me;
+const myFun = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (me = 0; me < 3; me++) {
+  console.log(myFun[me]);
+}
