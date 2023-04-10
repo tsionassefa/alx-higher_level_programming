b69bc12d0c42600a78ade4367e057707b3e26cb0
tsionassefa <tsionassefa101@gmail.com>
2023-04-10 15:55:18 +0300

    done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// A script that print arguments based on there number
+
+let myVar = process.argv.length;
+if (myVar > 3){
+	console.log('Arguments found');
+}else if (myVar === 3){
+	console.log('Argument found');
+}else{
+	console.log('No argument');
+}
