fad014eb11a9b330e663ac8eac740b0b4c57d017
tsionassefa <tsionassefa101@gmail.com>
2023-04-10 16:02:06 +0300

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,11 +2,11 @@
 
 // A script that print arguments based on there number
 
-let myVar = process.argv.length;
-if (myVar > 3){
-	console.log('Arguments found');
-}else if (myVar === 3){
-	console.log('Argument found');
-}else{
-	console.log('No argument');
+const myVar = process.argv.length;
+if (myVar > 3) {
+  console.log('Arguments found');
+} else if (myVar === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
