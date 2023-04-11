04901fdfb1be83a19840dee8d0d54a62813ed8d5
tsionassefa <tsionassefa101@gmail.com>
2023-04-11 16:22:49 +0300

    done

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,11 @@
 #!/usr/bin/node
-class Rectangle {
-	constructor (h, w) {
-		if (h <= 0 && w <= 0) {
-			this.height = undefined;
-			this.width = undefined;
-		} else {
-			this.height = h;
-			this.width = w;
-		}
-	}
-
-print() {
-	let stringDisp = '';
-    for (let x = 0; x < this.width; x++) {
-    for (let y = 0; y < this.height; y++) {
-        stringDisp += 'X';
-      }
-      console.log(stringDisp);
-      stringDisp = '';
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
-}
+};
