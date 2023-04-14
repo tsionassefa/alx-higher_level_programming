c71514ab073c98316799cdcf5a2092c2d6f52961
tsionassefa <tsionassefa101@gmail.com>
2023-04-14 11:23:28 +0300

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all State objects
+lists all State objects
 from the database.
 """
 
@@ -11,7 +11,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to the database and get the states 
+    Access to the database and get the states
+    from the database.
     """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
