a3895cb8a29c7c97a5e8b50a911bb434c55a7a32
tsionassefa <tsionassefa101@gmail.com>
2023-04-14 12:47:39 +0300

    done

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,15 +18,15 @@ if __name__ == "__main__":
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine(db_uri)
+    Session = sessionmaker(bind=eng)
 
-    session = Session()
+    ses = Session()
 
-    query = session.query(City, State).join(State)
+    query = ses.query(City, State).join(State)
 
     for _c, _s in query.all():
         print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
 
-    session.commit()
-    session.close()
+    ses.commit()
+    ses.close()
