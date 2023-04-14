d077e1e5b46a24af79e0f794998c3be318eba569
tsionassefa <tsionassefa101@gmail.com>
2023-04-14 10:17:52 +0300

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-list all the scripts from the 
-selected database 
+lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -9,14 +9,12 @@ from sys import argv
 
 if __name__ == '__main__':
 
-  db = MYSQLdb.connect(host="localhost", user=argv[1], port=3306,passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-  cur = db.cursor()
-  cur.execute("SELECT * FROM states")
-  rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-  for i in rows:
-      print(i)
-
-
-
+    for row in rows:
+        print(row)
