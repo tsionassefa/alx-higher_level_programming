91c703398e80b91183e2c1195bb8652820806119
tsionassefa <tsionassefa101@gmail.com>
2023-04-28 10:42:37 +0300

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-# a script that featches the content of url
+"""  a script that featches the content of url"""
 import urllib.request
+
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-
     with urllib.request.urlopen(url) as response:
         html = response.read()
-
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
