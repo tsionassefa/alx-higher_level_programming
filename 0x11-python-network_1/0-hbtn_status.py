c4c4522db86624e52ae9acc28894c890ed7fafdd
tsionassefa <tsionassefa101@gmail.com>
2023-04-28 10:12:07 +0300

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+#a script that featches the content of url
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
